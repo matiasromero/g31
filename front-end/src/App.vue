28ca0f8ee9880e7ff7e4752e37f2ccbafd2fd4c7
Ezequiel Casada <ezequielcasada@GLEBA.NET>
2019-05-07 16:10:00 -0300

    front-end

---
+++ front-end/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="col-lg-12">
+
+      <header id="header" class="col-lg-12">
+          <h1>Home SWITCHhome </h1>
+      </header>
+
+      <nav id="nav" class="col-lg-12">
+          <ul>
+            <router-link tag="li" router-link-active to="/"><a>Home</a></router-link>
+            <router-link tag="li" router-link-active to="/acerca-de"><a>Acerca de... </a></router-link>
+          </ul>
+      </nav>
+
+      <section id="main" class="col-lg-12">
+        <!-- Componente actual -->
+        <router-view></router-view>
+      </section>
+
+      <footer id="footer">
+          <p>FOOTER</p>
+      </footer>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
---
+++ front-end/src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+      <h1>Acerca de...</h1>
+      <p>Pagina About</p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
