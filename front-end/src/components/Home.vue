7a2995f514e0d3a15f16226b2c81f168178ed036
Ezequiel Casada <ezequielcasada@GLEBA.NET>
2019-05-21 11:43:39 -0300

    ver subasta Home.vue

--- front-end/src/components/Home.vue
+++ front-end/src/components/Home.vue
@@ -12,17 +12,23 @@
             <div class="card "  >
                 <div class="card-header"><i class="fa fa-fw fa-hammer"></i> <strong> Subastas</strong>  </div>
                 <div class="card-body">
-                    <div v-for="post in posts" v-bind:key="post.id"   id="residence">
+                    <div v-for="post in posts" :key= post.id  id="residence">
                         <div class="row">
                             <div class="col-sm-6">
                                 <img v-bind:src="post.thumbnailUrl"   width="150" height="150"/> 
                             </div>
                             <div class="col-sm-2">
+                                <form>
                                 	<a href="" class="btn btn-danger"><i class="fa fa-fw fa-certificate"></i> Pujar</a>
+                                    <br><br>
+                                    <input type="number" name="amount" v-model="amount" class="form-control input-sm" placeholder="Monto">
+                                </form>
                             </div>
+                            <div class="vl"></div>
                              <div class="col-sm-2">
                                 	<h4>Valor Actual </h4>
                                     <h3>$999.99</h3>
+                                    <a href="detalleSubasta">Detalles</a>
                             </div>
                         </div>
                     <hr>
@@ -31,11 +37,8 @@
                    
                 </div>
             </div>
-        </div>
-        
+        </div> 
     </div>
-
-
 </div>
 
 </template>
@@ -52,7 +55,7 @@ export default{
     name: 'Home',
     data() {
         return {
-            post: []
+            posts: []
         }
     },
     components: {
@@ -62,7 +65,7 @@ export default{
         let vue = this;
         axios.get('https://jsonplaceholder.typicode.com/photos')
         .then( function( response ) {
-            vue. posts = response.data;
+            vue.posts = response.data;
             console.log(vue.posts);
         }) 
     
@@ -82,6 +85,12 @@ export default{
     #residences{
        
         padding: 70px;
+    }
+
+    .vl {
+         border-left: 1px solid grey;
+        height: 150px;
+        margin-right: 20px;
     }
 
 </style>
